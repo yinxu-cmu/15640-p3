17c374d5be5d0ccd0e40458128d955f283c533b6
roddykou <roddykou@gmail.com>
2013-11-02 11:10:34 -0400

    jave project created

---
+++ src/dfs/YZFS.java
@@ -0,0 +1,10 @@
+package dfs;
+
+public class YZFS {
+
+	/* to run, go to bin/ directory, type java dfs.YZFS */
+	public static void main(String[] args) {
+		System.out.println("YZFS started");
+	}
+
+}
