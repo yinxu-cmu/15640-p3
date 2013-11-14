0dcf7866c09a453d66b36669a5f0ec19bacd7dd7
roddykou <roddykou@gmail.com>
2013-11-14 04:02:32 -0500

    both example works with soft coded conf

--- bin/s1.sh
+++ bin/s1.sh
@@ -1,4 +1,4 @@
-java dfs.YZFS -c 128.2.251.139 & 
+java dfs.YZFS -c 128.2.251.196 & 
 sleep 1
-java mapreduce.MapReduceSlave start &
+java mapreduce.MapReduceSlave &
 
--- bin/s2.sh
+++ bin/s2.sh
@@ -1,8 +1,8 @@
-java dfs.YZFS -c 128.2.251.139 & 
+java dfs.YZFS -c 128.2.251.196 & 
 sleep 1
-java mapreduce.MapReduceSlave start &
+java mapreduce.MapReduceSlave &
 sleep 1
 java dfs.YZFS -yzfs copyFromLocal ./inputs
 sleep 1
-java mapreduce.YZHadoop WordCount
-
+# java mapreduce.YZHadoop WordCount
+java mapreduce.YZHadoop Maximum
